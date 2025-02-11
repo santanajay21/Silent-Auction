f67ffbd694716e9e4093177c0d18e03bc17f64f1
Jeremy Santana <72940290+santanajay21@users.noreply.github.com>
2025-02-10 22:06:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# TODO-1: Ask the user for input
+name= input("What is your name:\n")
+bid = input("what is your bid:\n")
+any_more = input("Are there any other bidder? Type 'yes' or 'no'. \n")
+# TODO-2: Save data into dictionary {name: price}
+bidder_saved = {
+    name:bid
+}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
