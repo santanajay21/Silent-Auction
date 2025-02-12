1dc08822408c96fd1828f6ff2fcfa876ffb73532
Jeremy Santana <72940290+santanajay21@users.noreply.github.com>
2025-02-12 17:28:03 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ print(logo)
 def comparing_bids(bidding_dictionary):
     winner = ""
     highest_bid = 0 
-
     #max bid
-
     for bidder in bidding_dictionary:
         # this is how we get the value in a dictionary when looping through it
         bid_amount = bidding_dictionary[bidder]
@@ -24,3 +22,21 @@ def comparing_bids(bidding_dictionary):
 
 #  create a dictionary to store the bids
 bids = {}
+
+continue_bidding = True
+# while loop to keep asking for bids
+while continue_bidding:
+    name = input("What is your name?")
+    price = int(input("What is your bid? $"))
+    bids[name] = price
+    any_more_bids = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if any_more_bids == "no":
+        continue_bidding = False
+        comparing_bids(bids)
+    elif any_more_bids == "yes":
+        continue_bidding = True
+        print("\n" * 100)
+    else:
+        print("Invalid input. Please try again.")
+        continue_bidding = False
+        comparing_bids(bids)
